a0ffdf7c8d33d2ef0e575b8921c01f0e0af4ebb8
alakoshko <alakoshko@gmail.com>
2018-11-25 21:12:24 +0300

    * добавлен фонарик в канвас, * создана Views/FlashLightChargeView.cs

--- Unity3D_Lev2/Assets/ProjectsAssets/FPS/Scripts/Models/FlashLightModel.cs
+++ Unity3D_Lev2/Assets/ProjectsAssets/FPS/Scripts/Models/FlashLightModel.cs
@@ -1,17 +1,28 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 namespace FPS
 {
     public class FlashLightModel : MonoBehaviour
     {
+        public event Action<float> ChargeAmountChanged;
         public event Action<bool> FlashLightStateChanged;
+        public float ChargeAmount { get; private set; }
+
+        [SerializeField]
+        private float _drainMult = 3f;
+        [SerializeField]
+        private float _rechargeTime = 5f;
 
         private Light _light;
 
         public bool IsOn { get { return _light.enabled; } }
 
-        private void Awake() { _light = GetComponent<Light>(); }
+        private void Awake() {
+            _light = GetComponent<Light>();
+            ChargeAmount = 1f;
+        }
 
         public void On() {
             _light.enabled = true;
@@ -20,6 +31,25 @@ namespace FPS
         public void Off() {
             _light.enabled = false;
             if (FlashLightStateChanged != null) FlashLightStateChanged.Invoke(false);
+        }
+
+        private const float chargeDelay = .3f;
+        private IEnumerator ChangeFill()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(chargeDelay);
+                if (IsOn)
+                {
+                    ChargeAmount = Mathf.Clamp01(ChargeAmount - 1f / (Mathf.Max(0.01f, _rechargeTime * _drainMult) * chargeDelay));
+                    if (ChargeAmount <= 0f) Off();
+                }
+                else
+                {
+                    ChargeAmount = Mathf.Clamp01(ChargeAmount + 1f / (Mathf.Max(0.01f, _rechargeTime) * chargeDelay));
+                }
+                if (ChargeAmountChanged != null) ChargeAmountChanged.Invoke(ChargeAmount);
+            }
         }
     }
 }
