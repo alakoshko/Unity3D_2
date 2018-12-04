6f6b4c377f8ffa4e9922495452e60819a733df03
alakoshko <alakoshko@gmail.com>
2018-12-05 02:11:57 +0300

    * Дракон убивается

--- Unity3D_Lev2/Assets/ProjectsAssets/FPS/Scripts/Characters/Character.cs
+++ Unity3D_Lev2/Assets/ProjectsAssets/FPS/Scripts/Characters/Character.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
+using UnityEngine.Video;
 
 namespace FPS
 {
@@ -10,24 +12,48 @@ namespace FPS
         private float _maxHealth;
         public float MaxHealth => _maxHealth;
 
-        [SerializeField]
         private float _currentHealth;
         public float CurrentHealth => _currentHealth;
+
+        private Animator m_Animator;
+        [SerializeField]
+        private string _animatorDamage;
+        [SerializeField]
+        private string _animatorAttack;
+        [SerializeField]
+        private string _animatorDead;
+        [SerializeField]
+        private string _animatorMove;
+        [SerializeField]
+        private string _animatorWait;
+
+        public void Start()
+        {
+            _currentHealth = _maxHealth;
+            m_Animator = GetComponent<Animator>();
+        }
 
         public void ApplyDamage(float damage)
         {
             if (CurrentHealth <= 0) return;
             _currentHealth -= damage;
-            //Color = Random.ColorHSV();
+            m_Animator?.Play(_animatorDamage);
+
+            Debug.Log($"Current health: {_currentHealth}");
 
             if (CurrentHealth <= 0) Die();
         }
 
         public void Die()
         {
-            //Color = Color.red;
             Collider.enabled = false;
-            Rigidbody.useGravity = true;
+            var rb = GetComponent<Transform>().gameObject.AddComponent<Rigidbody>();
+            rb.useGravity = true;
+            rb.mass = _maxHealth / 2;
+            rb.AddForce(Vector3.up * Random.Range(10f, 30f), ForceMode.Impulse);
+
+            m_Animator?.Play(_animatorDead);
+            m_Animator?.Play(_animatorDead);
             Destroy(gameObject, 5f);
         }
     }
