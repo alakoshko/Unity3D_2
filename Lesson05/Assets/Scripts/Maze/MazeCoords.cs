5395e0b0da82438662ad5a51c0906f384cbac66b
alakoshko <alakoshko@gmail.com>
2018-12-12 00:18:38 +0300

    * Лабиринт из хабра

---
+++ Lesson05/Assets/Scripts/Maze/MazeCoords.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Maze.Editor
+{
+    public class MazeCoords
+    {
+        public MazeCoords(W4Maze maze)
+        {
+            for(int i = 0; i < maze.ColumnCount; i++)
+            {
+
+            }
+        }
+    }
+}
