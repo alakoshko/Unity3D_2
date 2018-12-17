4934458f59d529fab102d1b203377c877923dcfd
alakoshko <alakoshko@gmail.com>
2018-12-18 02:07:22 +0300

    * Старт урок07

---
+++ Unity3D_Lev2/Assets/ProjectsAssets/FPS/Scripts/GameManagement/GraphicsOptions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class GraphicsOptions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Debug.Log(Application.platform);
+
+#if UNITY_ANDROID || UNITY_IOS
+        Application.targetFrameRate = 30;
+#elif UNITY_EDITOR
+        Application.targetFrameRate = 60;
+#endif
+    }
+	
+}
